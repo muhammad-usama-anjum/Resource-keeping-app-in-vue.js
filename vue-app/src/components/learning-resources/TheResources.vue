bfdb3d4cf9d0c3903da70a95919043c1d1479c75
muhammad-usama-anjum <usamaanjum67@gmail.com>
2024-06-27 16:58:06 +0500

    created a resource keeping app in vue js

---
+++ vue-app/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <base-card>
+    <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
+    </base-card>
+    <component :is="selectedTab"></component>
+    </div>
+    
+</template>
+
+<script>
+import AddResource from './AddResource.vue';
+import StoredResources from './StoredResources.vue';
+export default {
+    components:{
+        StoredResources,
+        AddResource
+
+    },
+    data() {
+        return{
+            selectedTab: 'stored-resources',
+
+            storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        }
+      ],
+
+        };
+    },
+    provide(){
+        return{
+            resources: this.storedResources,
+            addResource: this.addResource
+        }
+    },
+    computed:{
+        storedResButtonMode(){
+            return this.selectedTab==='stored-resources' ? null : 'flat';
+        },
+        addResButtonMode(){
+            return this.selectedTab==='add-resource' ? null : 'flat';
+        },
+    },
+    methods:{
+        setSelectedTab(tab){
+            this.selectedTab=tab;
+        },
+        addResource(title,description,url)
+        {
+            const newResource={
+                id:new Date().toISOString(),
+                title: title,
+                description:description,
+                link:url
+            };
+                this.storedResources.unshift(newResource);
+                this.selectedTab='stored-resources';
+        }
+    }
+}
+</script>
